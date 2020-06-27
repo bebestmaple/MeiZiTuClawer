5887c5c36bdef4d1ca64a057eefbeea35053e081
hoyinngai@gmail.com <hoyinngai@gmail.com>
2020-06-27 23:08:34 +0800

    Initial commit​

---
+++ MeiZiTu/MeiZiTu/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MeiZiTu
+{
+    class Program
+    {
+        private static readonly string basePath = Directory.GetCurrentDirectory();
+        static void Main(string[] args)
+        {
+            ProcessAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+            Console.WriteLine("抓取完毕，按任意键退出");
+            Console.ReadKey();
+        }
+
+
+        static Task ProcessAsync()
+        {
+            const string baseUrl = "https://www.mzitu.com/";
+
+
+        }
+    }
+}
